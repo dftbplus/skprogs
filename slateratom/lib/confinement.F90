fdf5edf2a7bcfcb196f6fb178aa0960f9278b0ea
Tammo van der Heide <vanderhe@uni-bremen.de>
2024-08-25 21:38:22 +0200

    Address the feedback of the code review
    Co-authored-by: Ben Hourahine <benjamin.hourahine@strath.ac.uk>

--- slateratom/lib/confinement.F90
+++ slateratom/lib/confinement.F90
@@ -1,6 +1,6 @@
 #:include 'common.fypp'
 
-!> Module that builds up various supervectors.
+!> Module that builds up various confining potential supervectors.
 module confinement
 
   use common_accuracy, only : dp
@@ -454,61 +454,5 @@ contains
     pot = ww / (1.0 + exp(-aa * (rr - r0)))
 
   end function getWSPotential
-
-
-  ! !> Calculates analytic matrix elements of confining potential.
-  ! !! No checking for power, e.g. power==0 or power<0 etc. !
-  ! subroutine getConfPower_analytical(this, max_l, num_alpha, alpha, poly_order, vconf_matrix)
-
-  !   !> instance
-  !   class(TPowerConf), intent(in) :: this
-
-  !   !> maximum angular momentum
-  !   integer, intent(in) :: max_l
-
-  !   !> number of exponents in each shell
-  !   integer, intent(in) :: num_alpha(0:)
-
-  !   !> basis exponents
-  !   real(dp), intent(in) :: alpha(0:,:)
-
-  !   !> highest polynomial order + l in each shell
-  !   integer, intent(in) :: poly_order(0:)
-
-  !   !> confinement supervector
-  !   real(dp), intent(out) :: vconf_matrix(0:,:,:)
-
-  !   !! temporary storage
-  !   real(dp) :: alpha1
-
-  !   !! auxiliary variables
-  !   integer :: ii, jj, kk, ll, mm, nn, oo, nlp, nlq
-
-  !   vconf_matrix(:,:,:) = 0.0_dp
-
-  !   do ii = 0, max_l
-  !     if (this%power(ii) > 1.0e-06_dp) then
-  !       nn = 0
-  !       do jj = 1, num_alpha(ii)
-  !         do ll = 1, poly_order(ii)
-  !           nn = nn + 1
-  !           oo = 0
-  !           nlp = ll + ii
-  !           do kk = 1, num_alpha(ii)
-  !             alpha1 = 0.5_dp * (alpha(ii, jj) + alpha(ii, kk))
-  !             do mm = 1, poly_order(ii)
-  !               oo = oo + 1
-  !               nlq = mm + ii
-  !               vconf_matrix(ii, nn, oo) = 1.0_dp / sqrt(v(alpha(ii, jj), 2 * nlp)&
-  !                   & * v(alpha(ii, kk), 2 * nlq)) / (this%r0(ii) * 2.0_dp)**this%power(ii)&
-  !                   & * v(alpha1, nlp + nlq + this%power(ii))
-  !             end do
-  !           end do
-  !         end do
-  !       end do
-  !     end if
-  !   end do
-
-  ! end subroutine TConf_getConfPower_analytical
 
 end module confinement
